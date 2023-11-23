c9a5bc9c102cdc75915cac0f3ed2b76f8eea240a
stringsplit <77242831+nbitzz@users.noreply.github.com>
2023-11-23 08:57:20 -0800

    horrors

--- src/logo_generator/App.svelte
+++ src/logo_generator/App.svelte
@@ -2,17 +2,20 @@
     import Logo from "./elements/Logo.svelte"
 
     let logo: Logo
-    /*
+    
     let strips: [string[], string[], string[]] = [
         ["#FB405A", "#7A2259"],
         ["#7A2259", "#FB405A"],
         ["#FB405A", "#7A2259"]
-    ]*/
+    ]
+    /*
     let strips: [string[], string[], string[]] = [
         ["#5BCEFA", "#F5A9B8", "#FFFFFF", "#F5A9B8", "#5BCEFA"],
-        ["#770088", "#004CFF", "#028121", "#FFEE00", "#FF8D00", "#E50000"],
-        ["#2D2D2D", "#9B59D0", "#FFFFFF", "#FFF433"]
+        ["#E50000", "#FF8D00", "#FFEE00", "#028121", "#004CFF", "#770088" ],
+        // ["#2D2D2D", "#9B59D0", "#FFFFFF", "#FFF433"] enby
+        ["#A30262", "#D362A4", "#FFFFFF", "#FF9A56", "#D52D00"]
     ]
+    */
 </script>
 
 <div id="main">
@@ -21,16 +24,19 @@
     <div id="stripBuilder">
         {#each strips as strip}
             <div class="strip">
-                <div class="item"><input type="color" bind:value={strip[0]}></div>
-                <!-- stupid but who gives a fuck -->
-                {#each Array.from(strip.keys()).slice(1,-1) as index}
+                {#each strip.keys() as index}
+                    <!-- too lazy to attempt fixing so you get -->
+                    {#if index == strip.length-1}
+                        <button on:click={() => {strip.splice(strip.length-1,0,"#FFFFFF"); strips = strips}}>+</button>
+                    {/if}
+
                     <div class="item">
                         <input type="color" bind:value={strip[index]}>
-                        <button on:click={() => {strip.splice(index,1); strips = strips}}>X</button>
+                        {#if index != strip.length-1 && index != 0}
+                            <button on:click={() => {strip.splice(index,1); strips = strips}}>X</button>
+                        {/if}
                     </div>
                 {/each}
-                <button on:click={() => {strip.splice(strip.length-1,0,"#FFFFFF"); strips = strips}}>+</button>
-                <div class="item"><input type="color" bind:value={strip[strip.length-1]}></div>
             </div>
         {/each}
     </div>
